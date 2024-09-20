80aa52baa83b36358f4c82718c82c4f83c3a856c
Rasheed <119102828+Rasheed-ibn-Rafi@users.noreply.github.com>
2024-09-20 15:34:57 +0530

    init: initial project folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="grid place-items-center w-screen h-screen bg-blue-300">
+      <p className="text-3xl font-semibold tracking-wider text-gray-700 text-center leading-relaxed">
+        This chat app is under construction <br />
+        server is getting ready now
+      </p>
+    </div>
+  );
+}
+
+export default App;
